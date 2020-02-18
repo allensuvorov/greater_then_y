4d0fb25e1bdd9be58bc0636900d43ffa45449062
Allen <allenvs@mail.ru>
2020-02-18 21:44:05 +0300

    added two parameters to function

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-var arr = [1, 3, 5, 7];
 var count = 0;
-function greater_than_y (y) {
+function greater_than_y (y, arr) {
     for (let i=0;i<arr.length;i++) {
         if (arr[i]>y) count++;
     };
     console.log(count)
 };
-greater_than_y(2);
+
+var x = 2;
+var user_arr = [1, 3, 5, 7];
+
+greater_than_y(x, user_arr);
